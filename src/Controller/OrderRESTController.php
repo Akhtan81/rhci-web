174addd2c0fd9f33d239e8e712f2248779ce2865
Gram <gram7gram@gmail.com>
2018-08-29 09:03:46 +0300

    #master in progress

--- src/Controller/OrderRESTController.php
+++ src/Controller/OrderRESTController.php
@@ -3,12 +3,81 @@
 namespace App\Controller;
 
 use App\Service\OrderService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class OrderRESTController extends Controller
 {
+    public function getsAction(Request $request)
+    {
+        $filter = $request->get('filter', []);
+        $page = $request->get('page', 1);
+        $page = intval($page <= 0 ? 1 : $page);
+        $limit = $request->get('limit', 10);
+        $limit = intval($limit < 0 ? 10 : $limit);
+
+        $service = $this->get(OrderService::class);
+        $user = $this->get(UserService::class)->getUser();
+
+        $filter['user'] =  $user->getId();
+
+        try {
+
+            $total = $service->countByFilter($filter);
+            $items = [];
+
+            if ($total > 0) {
+                $entities = $service->findByFilter($filter, $page, $limit);
+
+                $items = $service->serialize($entities);
+            }
+
+            return new JsonResponse([
+                'page' => $page,
+                'limit' => $limit,
+                'total' => $total,
+                'count' => count($items),
+                'items' => $items
+            ]);
+
+        } catch (\Exception $e) {
+
+            return new JsonResponse([
+                'message' => $e->getMessage()
+            ], $e->getCode() > 300 ? $e->getCode() : JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getAction($id)
+    {
+        $service = $this->get(OrderService::class);
+        $user = $this->get(UserService::class)->getUser();
+
+        try {
+
+            $entity = $service->findOneByFilter([
+                'id' => $id,
+                'user' => $user->getId()
+            ]);
+            if (!$entity) {
+                throw $this->createNotFoundException();
+            }
+
+            $item = $service->serialize($entity);
+
+
+            return new JsonResponse($item);
+
+        } catch (\Exception $e) {
+
+            return new JsonResponse([
+                'message' => $e->getMessage()
+            ], $e->getCode() > 300 ? $e->getCode() : JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
     public function postAction(Request $request)
     {
         $content = json_decode($request->getContent(), true);
--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -12,18 +12,38 @@ class OrderRepository extends EntityRepository
      * @param array $filter
      * @param int $page
      * @param int $limit
+     *
      * @return array
      */
     public function findByFilter($filter = [], $page = 0, $limit = 0)
     {
         $qb = $this->createFilterQuery($filter);
 
-        $qb->orderBy('order.createdAt', 'DESC');
+        $qb->orderBy('entity.createdAt', 'DESC');
+
+        $qb->select('entity.id')->distinct(true)
+            ->addSelect('entity.createdAt');
 
         if ($page > 0 && $limit > 0) {
             $qb->setMaxResults($limit)
                 ->setFirstResult($limit * ($page - 1));
         }
+
+        $result = $qb->getQuery()
+            ->useQueryCache(true)
+            ->getArrayResult();
+
+        if (count($result) === 0) return [];
+
+        $ids = array_map(function ($item) {
+            return $item['id'];
+        }, $result);
+
+        $qb = $this->createFilterQuery([
+            'ids' => $ids
+        ]);
+
+        $qb->orderBy('entity.createdAt', 'DESC');
 
         return $qb->getQuery()
             ->useQueryCache(true)
@@ -33,33 +53,45 @@ class OrderRepository extends EntityRepository
 
     private function createFilterQuery($filter = [])
     {
-        $qb = $this->createQueryBuilder('order');
+        $qb = $this->createQueryBuilder('entity');
         $e = $qb->expr();
 
         $qb
             ->addSelect('user')
             ->addSelect('partner')
-            ->addSelect('category')
-            ->addSelect('partnerCategory')
             ->addSelect('district')
             ->addSelect('updatedBy')
-        ;
+            ->addSelect('message')
+            ->addSelect('messageUser')
+            ->addSelect('messageMedia')
+            ->addSelect('media')
+            ->addSelect('item')
+            ->addSelect('category')
+            ->addSelect('partnerCategory');
 
         $qb
-            ->join('order.user', 'user')
-            ->leftJoin('order.partner', 'partner')
-            ->leftJoin('order.category', 'category')
-            ->leftJoin('order.partnerCategory', 'partnerCategory')
-            ->leftJoin('order.district', 'district')
-            ->leftJoin('order.updatedBy', 'updatedBy')
-        ;
+            ->join('entity.user', 'user')
+            ->join('entity.messages', 'message')
+            ->join('message.user', 'messageUser')
+            ->leftJoin('message.media', 'messageMedia')
+            ->leftJoin('messageMedia.media', 'media')
+            ->join('entity.items', 'item')
+            ->join('item.category', 'category')
+            ->leftJoin('item.partnerCategory', 'partnerCategory')
+            ->leftJoin('entity.partner', 'partner')
+            ->leftJoin('entity.district', 'district')
+            ->leftJoin('entity.updatedBy', 'updatedBy');
 
         foreach ($filter as $key => $value) {
             if (!$value) continue;
 
             switch ($key) {
                 case 'id':
-                    $qb->andWhere($e->eq('order.id', ":$key"))
+                    $qb->andWhere($e->eq('entity.id', ":$key"))
+                        ->setParameter($key, $value);
+                    break;
+                case 'ids':
+                    $qb->andWhere($e->in('entity.id', ":$key"))
                         ->setParameter($key, $value);
                     break;
                 case 'user':
@@ -74,6 +106,7 @@ class OrderRepository extends EntityRepository
 
     /**
      * @param array $filter
+     *
      * @return int
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
@@ -82,7 +115,7 @@ class OrderRepository extends EntityRepository
         $qb = $this->createFilterQuery($filter);
         $e = $qb->expr();
 
-        $qb->select($e->countDistinct('order.id'));
+        $qb->select($e->countDistinct('entity.id'));
 
         return $qb->getQuery()
             ->useQueryCache(true)
