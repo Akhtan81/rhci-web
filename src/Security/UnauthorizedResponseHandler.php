53042d1386894debed8cd9c7afb44dc861042e52
Gram <gram7gram@gmail.com>
2018-09-08 17:28:24 +0300

    #master in progress

--- src/Security/UnauthorizedResponseHandler.php
+++ src/Security/UnauthorizedResponseHandler.php
@@ -3,6 +3,7 @@
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
@@ -12,8 +13,12 @@ class UnauthorizedResponseHandler implements AuthenticationEntryPointInterface
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        return new JsonResponse([
-            'message' => 'unauthorized'
-        ], JsonResponse::HTTP_UNAUTHORIZED);
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse([
+                'message' => 'unauthorized'
+            ], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        return new RedirectResponse('/login');
     }
 }
