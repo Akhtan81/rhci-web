ada91af06f1b3da4a70d6cb6c382dfff8a69e833
Gram <gram7gram@gmail.com>
2018-09-22 14:47:48 +0300

    #master in progress

--- spa/src/PartnerEdit/sagas/Validation.js
+++ spa/src/PartnerEdit/sagas/Validation.js
@@ -1,5 +1,5 @@
 import {all, takeEvery, put, select, throttle} from 'redux-saga/effects'
-import {MODEL_CHANGED, VALIDATE_REQUEST, UPLOAD_MEDIA_SUCCESS} from '../actions'
+import {MODEL_CHANGED, VALIDATE_REQUEST, UPLOAD_MEDIA_SUCCESS, FETCH_SUCCESS} from '../actions'
 import Validate from '../actions/Validate'
 
 function* requestValidation() {
@@ -17,6 +17,10 @@ function* runValidation() {
 export default function* sagas() {
     yield all([
         throttle(400, MODEL_CHANGED, requestValidation),
+
+        takeEvery([
+            FETCH_SUCCESS,
+        ], requestValidation),
 
         takeEvery([
             VALIDATE_REQUEST,
