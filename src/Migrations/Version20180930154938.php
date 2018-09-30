5f0f7aa4c42fa98d0d34b5669ced99ef53155c2d
Gram <gram7gram@gmail.com>
2018-09-30 19:47:52 +0300

    #master in progress

---
+++ src/Migrations/Version20180930154938.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180930154938 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE users DROP CONSTRAINT fk_1483a5e964d218e');
+        $this->addSql('DROP INDEX uniq_1483a5e964d218e');
+        $this->addSql('ALTER TABLE users DROP location_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE users ADD location_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE users ADD CONSTRAINT fk_1483a5e964d218e FOREIGN KEY (location_id) REFERENCES user_locations (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE UNIQUE INDEX uniq_1483a5e964d218e ON users (location_id)');
+    }
+}
