b4ccd48857b504abdc427a53d581bd7a675116b9
Gram <gram7gram@gmail.com>
2018-10-26 10:43:56 +0300

    #subdomain in progress

---
+++ admin/src/Migrations/Version20181026074155.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\PartnerStatus;
+use App\Entity\SubscriptionType;
+use App\Service\PartnerService;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181026074155 extends AbstractMigration implements ContainerAwareInterface
+{
+    /** @var ContainerInterface */
+    private $container;
+
+    public function up(Schema $schema): void
+    {
+
+        $partnerService = $this->container->get(PartnerService::class);
+        $em = $this->container->get('doctrine')->getManager();
+
+        $partners = $partnerService->findByFilter([
+            'status' => PartnerStatus::APPROVED
+        ], 1, 5);
+        foreach ($partners as $partner) {
+            $partnerService->createCustomer($partner);
+
+            $em->persist($partner);
+        }
+
+        $em->flush();
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+
+    /**
+     * Sets the container.
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
