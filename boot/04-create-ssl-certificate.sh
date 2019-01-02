20379f91da5040dc6decefe2b98a761703219668
Gram <gram7gram@gmail.com>
2019-01-02 21:25:05 +0200

    #master hotfix - Fixed ssl renewal instructions

--- boot/04-create-ssl-certificate.sh
+++ boot/04-create-ssl-certificate.sh
@@ -1,16 +1,20 @@
 PROJECT_DIR=/var/www/rhci-web
+OWNER=gram7gram.work@gmail.com
 
-docker run -it --rm --name certbot \
+sudo docker run -it --rm --name certbot \
+            -v "$PROJECT_DIR/www/public:/usr/share/nginx/html/public" \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            certbot/certbot certonly --webroot --webroot-path /usr/share/nginx/html/public \
+            -d mobilerecycling.net \
+            -m $OWNER
+
+sudo docker run -it --rm --name certbot \
             -v "$PROJECT_DIR/admin/public:/var/www/html" \
             -v "/etc/letsencrypt:/etc/letsencrypt" \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-            certbot/certbot certonly --webroot \
-			--webroot-path /var/www/html \
-			-d mobilerecycling.net \
-			-d admin.mobilerecycling.net \
-			-m gram7gram.work@gmail.com
-
-cp $PROJECT_DIR/admin/public/.well-known/acme-challenge/* \
-    $PROJECT_DIR/www/public/.well-known/acme-challenge
+            certbot/certbot certonly --webroot --webroot-path /var/www/html \
+            -d admin.mobilerecycling.net \
+            -m $OWNER
 
 cat $PROJECT_DIR/boot/cron/ssl-renew.conf | crontab -u root -
--- boot/05-ssl-certificate-renew.sh
+++ boot/05-ssl-certificate-renew.sh
@@ -3,13 +3,10 @@
 
 PROJECT_DIR=/var/www/rhci-web
 
-docker run -it --rm --name certbot \
+docker run --rm --name certbot \
             -v "$PROJECT_DIR/admin/public:/var/www/html" \
             -v "/etc/letsencrypt:/etc/letsencrypt" \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
             certbot/certbot renew
 
-cp $PROJECT_DIR/admin/public/.well-known/acme-challenge/* \
-    $PROJECT_DIR/www/public/.well-known/acme-challenge
-
-docker-compose restart admin www
+#docker-compose restart admin www
