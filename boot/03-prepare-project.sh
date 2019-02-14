279963af39fb93bde612161291e064a0022e547d
Gram <gram7gram@gmail.com>
2019-02-14 18:36:49 +0200

    #partner-categories hotfix

--- boot/03-prepare-project.sh
+++ boot/03-prepare-project.sh
@@ -2,6 +2,10 @@
 
 source admin/.env
 
+if [ -z "$DATABASE_NAME" ]; then echo "[-] Missing env variable DATABASE_NAME"; exit 1; fi;
+
+if [ -z "$UPLOAD_IMAGE_DIR" ]; then echo "[-] Missing env variable UPLOAD_IMAGE_DIR"; exit 1; fi;
+
 # Create containers
 docker-compose build
 
@@ -11,10 +15,13 @@ docker-compose up -d
 # Install dependencies
 ./php ./composer.phar install
 
-# Create database if not exists
-./php bin/console doctrine:database:create --if-not-exists
-
 # Create poster upload directory
 docker-compose exec admin sh -c "mkdir -p /var/www/html/public$UPLOAD_IMAGE_DIR && chmod 777 /var/www/html/public$UPLOAD_IMAGE_DIR"
 
-./update
+bash ./cache
+
+# Update database schema with new configuration
+./php bin/console doctrine:migrations:migrate -n
+
+# Insert default data into database
+docker-compose exec db psql -U postgres -d $DATABASE_NAME -a -f /var/www/html/provision.sql
