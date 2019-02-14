e2b3daad8c354eb5a01cb55941150110aada14c3
Gram <gram7gram@gmail.com>
2019-02-14 17:46:53 +0200

    #MRS-45 fixed

--- boot/03-prepare-project.sh
+++ boot/03-prepare-project.sh
@@ -11,10 +11,9 @@ docker-compose up -d
 # Install dependencies
 ./php ./composer.phar install
 
-# Create database if not exists
-./php bin/console doctrine:database:create --if-not-exists
-
 # Create poster upload directory
 docker-compose exec admin sh -c "mkdir -p /var/www/html/public$UPLOAD_IMAGE_DIR && chmod 777 /var/www/html/public$UPLOAD_IMAGE_DIR"
+
+./cache
 
 ./update
